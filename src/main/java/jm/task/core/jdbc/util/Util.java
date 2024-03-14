06a4c1cacca39418bbd1c0439d027e4745a85b86
kiran <ghalekiran03@gmail.com>
2024-03-15 03:04:54 +0900

    updated

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,6 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Util {
 
